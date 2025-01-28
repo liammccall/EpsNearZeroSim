5e1ebbe468276c20ab04c439bfdf023118f1ad34
Liam from Omega <lkm49@case.edu>
2025-01-28 16:53:49 +0000

    multistack looks like enz

--- tests/multistack.py
+++ tests/multistack.py
@@ -2,7 +2,7 @@
 import meep as mp
 import math
 
-cell = mp.Vector3(3000, 2000, 0)
+cell = mp.Vector3(1000, 1000, 0)
 
 #all nm
 wvl = 532
@@ -21,60 +21,62 @@ auK = 2.455
 auC = 2 * math.pi * freq * auK / auN
 auWidth = 10
 
-simLength = 3 / freq
-timeRes = simLength / 100
+simLength = 10 / freq
+timeRes = simLength / 1000
 
+
+#Uncomment to include APTMS 1 nm layers
 def createLayer(initialOffset, order):
         return [
         mp.Block(
             mp.Vector3(tio2Width, mp.inf, mp.inf),
             center=mp.Vector3((0 + order) * tio2Width + 
-                              (0 + 2 * order) * aptmsWidth + 
+                            #   (0 + 2 * order) * aptmsWidth + 
                               (0 + order) * auWidth + 
                               initialOffset, 0, 0),
             material=mp.Medium(epsilon=tio2N, D_conductivity=tio2C),
         ),
-        mp.Block(
-            mp.Vector3(aptmsWidth, mp.inf, mp.inf),
-            center=mp.Vector3((1 + order) * tio2Width + 
-                              (0 + 2 * order) * aptmsWidth + 
-                              (0 + order) * auWidth + 
-                              initialOffset, 0, 0),
-            material=mp.Medium(epsilon=aptmsN, D_conductivity=aptmsC),
-        ),
+        # mp.Block(
+        #     mp.Vector3(aptmsWidth, mp.inf, mp.inf),
+        #     center=mp.Vector3((1 + order) * tio2Width + 
+        #                       (0 + 2 * order) * aptmsWidth + 
+        #                       (0 + order) * auWidth + 
+        #                       initialOffset, 0, 0),
+        #     material=mp.Medium(epsilon=aptmsN, D_conductivity=aptmsC),
+        # ),
         mp.Block(
             mp.Vector3(auWidth, mp.inf, mp.inf),
             center=mp.Vector3((1 + order) * tio2Width + 
-                              (1 + 2 * order) * aptmsWidth + 
+                            #   (1 + 2 * order) * aptmsWidth + 
                               (0 + order) * auWidth + 
                               initialOffset, 0, 0),
             material=mp.Medium(epsilon=auN, D_conductivity=auC),
         ),
-        mp.Block(
-            mp.Vector3(aptmsWidth, mp.inf, mp.inf),
-            center=mp.Vector3((1 + order) * tio2Width + 
-                              (1 + 2 * order) * aptmsWidth + 
-                              (1 + order) * auWidth + 
-                              initialOffset, 0, 0),
-            material=mp.Medium(epsilon=aptmsN, D_conductivity=aptmsC),
-        )
+        # mp.Block(
+        #     mp.Vector3(aptmsWidth, mp.inf, mp.inf),
+        #     center=mp.Vector3((1 + order) * tio2Width + 
+        #                       (1 + 2 * order) * aptmsWidth + 
+        #                       (1 + order) * auWidth + 
+        #                       initialOffset, 0, 0),
+        #     material=mp.Medium(epsilon=aptmsN, D_conductivity=aptmsC),
+        # )
     ]
     
 geometry = []
 
-for i in range(0, 10, 1):
-    geometry.extend(createLayer(0.3, i))
+for i in range(0, 3, 1):
+    geometry.extend(createLayer(0, i))
 
 sources = [
     mp.Source(
-        mp.ContinuousSource(frequency=freq), component=mp.Ez, center=mp.Vector3(-200, 0)
+        mp.ContinuousSource(frequency=freq), component=mp.Ez, center=mp.Vector3(-100, 0)
     )
 ]
 
-pml_layers = [mp.PML(500)]
+pml_layers = [mp.PML(200)]
 
 
-resolution = 10
+resolution = 1
 
 sim = mp.Simulation(
     cell_size=cell,
@@ -82,7 +84,7 @@ sim = mp.Simulation(
     geometry=geometry,
     sources=sources,
     resolution=resolution,
-    Courant=0.1
+    Courant=0.2
 )
 
 import matplotlib.animation as ani
@@ -108,8 +110,8 @@ sim.reset_meep()
 f = plt.figure(dpi=100)
 
 def zoomin(ax):
-    ax.set_xlim(-1000, 1000)
-    ax.set_ylim(-500, 500)
+    ax.set_xlim(-100, 100)
+    ax.set_ylim(-100, 100)
     return ax
 
 ez_data = []
@@ -132,7 +134,7 @@ fig, ax = plt.subplots()
 
 # ani = ani.FuncAnimation(fig, animate, frames=range(len(ez_data)), interval=50, blit=True)
 # plt.show()
-Animate = mp.Animate2D(fields=mp.Ez, f=f, realtime=False, normalize=True, plot_modifiers=[zoomin], 
+Animate = mp.Animate2D(fields=mp.Ez, f=f, realtime=False, normalize=False, plot_modifiers=[zoomin], 
                            field_parameters={'alpha':0.8, 'cmap':'prism', 'interpolation':'spline36'})
 plt.close()
 
@@ -140,4 +142,4 @@ sim.run(mp.at_every(timeRes, Animate), until=simLength)
 plt.close()
 
 filename = "plots/multistack.mp4"
-Animate.to_mp4(5, filename)
+Animate.to_mp4(10, filename)
