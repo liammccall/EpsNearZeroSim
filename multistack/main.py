74bd3156ce688733a771c662e4a69628baa9ca13
Liam from Omega <lkm49@case.edu>
2025-02-13 23:26:31 +0000

    pair coupling test

--- multistack/main.py
+++ multistack/main.py
@@ -2,17 +2,46 @@ import numpy as np
 import multifunction
 import matplotlib.pyplot as plt
 
+import contextlib
+import io
+import sys
+
+@contextlib.contextmanager
+def nostdout():
+    save_stdout = sys.stdout
+    sys.stdout = io.StringIO()
+    yield
+    sys.stdout = save_stdout
+
 def main():
-    distances = np.linspace(5, 45, 10)
-    fluxes = []
+    distances = np.linspace(5, 100, 100)
+    total_fluxes = []
+    radiative_fluxes = []
     for dist in distances:
-        fluxes.append(multifunction.multi(532, dist, ""))
+        
+        print(dist)
+        
+        with nostdout():
+            total = np.abs(multifunction.multi(532, dist, ""))
+            
+        total_fluxes.append(total)
     
-    np.save("fluxes.npy", fluxes)
+    baseline_total= multifunction.multi(532, 0, "", True)
+    
+    total_fluxes = total_fluxes / np.abs(baseline_total)
+    
+    # np.save("baseline_total_rad.npy", [baseline_total, baseline_rad])
+    
+    np.save("total_fluxes.npy", total_fluxes)
     
     fig, ax = plt.subplots()
-    ax.plot(distances, fluxes)
-    fig.savefig("fluxplot.png")
+    ax.plot(distances, total_fluxes)
+    fig.savefig("single_eplot.png")
+    
+    # nonradiative_enhancement = total_fluxes - radiative_fluxes / (baseline_total - baseline_rad)
+    # fig, ax = plt.subplots()
+    # ax.plot(distances, nonradiative_enhancement)
+    # fig.savefig("nonradiative_fluxplot.png")
     
 if __name__ == "__main__":
     main()
