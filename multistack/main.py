973b44944a3824a8099358145b06fd598594c30d
liammccall <liamkm895@gmail.com>
2025-02-21 20:07:52 -0500

    checkpoint

--- multistack/main.py
+++ multistack/main.py
@@ -15,34 +15,16 @@ def nostdout():
 
 def main():
     distances = np.linspace(5, 100, 100)
-    total_x_real = []
-    total_x_imag = []
-    total_y_real = []
-    total_y_imag = []
-    total_z_real = []
-    total_z_imag = []
-    radiative_fluxes = []
+    ldos=[]
     for dist in distances:
         
         print(dist)
         
         with nostdout():
-            numx, numy, numz = multifunction.multi(532, dist, "")
-            x_real = np.power(np.real(numx), 2)
-            x_imag = np.power(np.imag(numx), 2)
-            y_real = np.power(np.real(numy), 2)
-            y_imag = np.power(np.imag(numy), 2)
-            z_real = np.power(np.real(numz), 2)
-            z_imag = np.power(np.imag(numz), 2)
-            
-            total_x_real.append(x_real)
-            total_x_imag.append(x_imag)
-            total_y_real.append(y_real)
-            total_y_imag.append(y_imag)
-            total_z_real.append(z_real)
-            total_z_imag.append(z_imag)
+            numx = multifunction.multi(532, dist, "")
+            ldos.append(numx)
     
-    baseline_total_x, baseline_total_y, baseline_total_z = multifunction.multi(532, 0, "", True)
+    baseline = multifunction.multi(532, 0, "", True)
     
     # total_fluxes_real = total_fluxes_real / np.real(baseline_total)
     # total_fluxes_imag = total_fluxes_imag / np.imag(baseline_total)
@@ -53,24 +35,9 @@ def main():
     # np.save("total_e_imag.npy", total_fluxes_imag)
     
     fig, ax = plt.subplots()
-    ax.plot(distances, total_x_real, label="x")
-    ax.plot(distances, total_y_real, label="y")
-    ax.plot(distances, total_z_real, label="z")
-    ax.axhline(np.power(np.real(baseline_total_x), 2), label="x")
-    ax.axhline(np.power(np.real(baseline_total_y), 2), label="y")
-    ax.axhline(np.power(np.real(baseline_total_z), 2), label="z")
-    ax.legend()
-    fig.savefig("single_eplot_real.png")
-
-    fig, ax = plt.subplots()
-    ax.plot(distances, total_x_imag, label="x")
-    ax.plot(distances, total_y_imag, label="y")
-    ax.plot(distances, total_z_imag, label="z")
-    ax.axhline(np.power(np.imag(baseline_total_x), 2), label="x")
-    ax.axhline(np.power(np.imag(baseline_total_y), 2), label="y")
-    ax.axhline(np.power(np.imag(baseline_total_z), 2), label="z")
-    ax.legend()
-    fig.savefig("single_eplot_imag.png")
+    ax.plot(distances, np.divide(ldos,baseline))
+    # ax.axhline(np.power(np.real(baseline), 2))
+    fig.savefig("ldos.png")
 
     # np.save("baseline_total.npy", [baseline_total])
     
